17ac5e2e1798d69809aff5828b96357e579788f8
Roei Ben Zion <roei.benzion@gmail.com>
2025-08-22 10:08:58 +0300

    try batching collate function

--- llava/eval/model_vqa_loader.py
+++ llava/eval/model_vqa_loader.py
@@ -83,7 +83,8 @@ def collate_fn(batch):
 def create_data_loader(questions, image_folder, tokenizer, image_processor, model_config, batch_size=1, num_workers=4, image_aspect_ratio=None):
     # assert batch_size == 1, "batch_size must be 1"
     dataset = CustomDataset(questions, image_folder, tokenizer, image_processor, model_config, anyres=(image_aspect_ratio == 'anyres'))
-    data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False, collate_fn=collate_fn)
+    # data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False, collate_fn=collate_fn)
+    data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
     return data_loader
 
 def create_7b_dups(path):
