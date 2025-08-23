e8a1cbd6ffa25072962a4a82d6cc4b55dff4ce1f
Roei Ben Zion <roei.benzion@gmail.com>
2025-08-23 15:51:59 +0300

    wa

--- llava/train/train.py
+++ llava/train/train.py
@@ -1081,7 +1081,7 @@ def train(attn_implementation=None):
         from llava.model.builder import load_pretrained_model
         from llava.mm_utils import get_model_name_from_path
         model_path = model_args.pretrain_mm_mlp_adapter
-        model_name = get_model_name_from_path(model_path)
+        model_name = 'llava-lora'
         load_pretrained_model(model_path, model_args.model_name_or_path, model_name)
         
     trainer = LLaVATrainer(model=model,
