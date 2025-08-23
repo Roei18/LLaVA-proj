81969248750a779ac2b1fd50aac91be23062718e
Roei Ben Zion <roei.benzion@gmail.com>
2025-08-23 11:50:30 +0300

    revert and fix other script

--- scripts/v1_5/fga_finetune_lora.sh
+++ scripts/v1_5/fga_finetune_lora.sh
@@ -5,11 +5,10 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path playground/data/eval/vizwiz/llava_test.jsonl \
-    --image_folder playground/data/eval/vizwiz/test \
+    --data_path ./playground/data/llava_v1_5_mix665k.json \
+    --image_folder ./playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-lora/mm_projector.bin \
-    --pretrain_lora ./checkpoints/llava-v1.5-7b-lora/adapter_model.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
