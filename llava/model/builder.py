36d5adacac9cf57763a053b2b822819d7f73c9f4
Roei Ben Zion <roei.benzion@gmail.com>
2025-09-01 08:26:58 +0300

    better log

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -81,6 +81,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
             print('Loading LLaVA from base model...')
             model = LlavaLlamaForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
+            prior_keys = list(model.state_dict().keys())
             token_num, tokem_dim = model.lm_head.out_features, model.lm_head.in_features
             if model.lm_head.weight.shape[0] != token_num:
                 model.lm_head.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
@@ -121,7 +122,8 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             incompat = model.load_state_dict(non_lora_trainables, strict=False)
             print(f"Missing keys: {len(incompat.missing_keys)}")
             print(f"Unexpected keys: {len(incompat.unexpected_keys)}")
-            for key in incompat.missing_keys:
+            missing_keys_except_the_loaded = [key for key in incompat.missing_keys if key not in prior_keys]
+            for key in missing_keys_except_the_loaded:
                 print(" -", key)
 
         elif model_base is not None:
